3130954d765deddb84a9c10f85138ad79bfc72be
OndrejM <ondrej.mihalyi@gmail.com>
2015-02-07 20:35:45 +0100

    Refactoring to simplify mapping function

--- src/main/java/eu/inginea/fizzbuzz/FizzBuzz.java
+++ src/main/java/eu/inginea/fizzbuzz/FizzBuzz.java
@@ -10,17 +10,17 @@ public class FizzBuzz {
     
     public static String generate(int toNumber) {
         return IntStream.rangeClosed(1, toNumber)
-                .mapToObj(n -> (divisibleBy3and5(n) ? "FizzBuzz"
-                            : divisibleBy3(n) ? "Fizz"
-                            : divisibleBy5(n) ? "Buzz"
-                            : String.valueOf(n))
-                )
+                .mapToObj(n -> {
+                            StringBuilder r = new StringBuilder();
+                            if (divisibleBy3(n)) r.append("Fizz");
+                            if (divisibleBy5(n)) r.append("Buzz");
+                            return (r.length() > 0) 
+                                    ? r.toString()
+                                    : String.valueOf(n);
+                })
                 .reduce("", (r, s) -> r.length() > 0 ? r + "," + s : s);
     }
 
-    private static boolean divisibleBy3and5(int n) {
-        return divisibleBy3(n) && divisibleBy5(n);
-    }
     private static boolean divisibleBy3(int n) {
         return n % 3 == 0;
     }
